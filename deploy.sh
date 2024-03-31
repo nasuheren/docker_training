862da0292311ae7d42a4e3ebb863916dce98c535
nasuheren <nasuherendemirci@gmail.com>
2024-03-31 17:33:20 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Docker pull
-res = $(docker image inspect nasuheren/dockertraining_flask:latest --format="exists")
-if ["$res" != "exists"]; then
+res=$(docker image inspect nasuheren/dockertraining_flask:latest --format="exists")
+if [ "$res" != "exists" ]; then
     docker pull nasuheren/dockertraining_flask:latest
 fi
 
 #stop and remove
-ce = $(docker ps -a --format "{{.Names}}" | grep "^dockertraining$")
-if ["$ce" != ""]; then
+ce=$(docker ps -a --format "{{.Names}}" | grep "^dockertraining$")
+if [ "$ce" != "" ]; then
     docker container stop $ce
     docker rm -f $ce
 fi
