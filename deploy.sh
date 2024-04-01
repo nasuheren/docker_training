f2f4be6ea87670fd6de878bb3237d6f753e8c43a
nasuheren <nasuherendemirci@gmail.com>
2024-04-01 16:53:59 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #Docker pull
+echo "docker pull"
 res=$(docker image inspect nasuheren/dockertraining:latest --format="exists")
 if [ "$res" != "exists" ]; then
     docker pull nasuheren/dockertraining:latest
 fi
 
 #stop and remove
+echo "echo stop and remove"
 ce=$(docker ps -a --format "{{.Names}}" | grep "^dockertraining$")
 if [ "$ce" != "" ]; then
     docker container stop $ce
--- first_start.sh
+++ first_start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # build docker image
+echo "docker build"
 docker build -t dockertraining .
 
 # image tag
+echo "docker image tag"
 docker tag dockertraining nasuheren/dockertraining:latest
 
 # push docker hub
+echo "docker push"
 docker push nasuheren/dockertraining:latest
 
 # deploy
+echo "deploy.sh"
 ./deploy.sh
