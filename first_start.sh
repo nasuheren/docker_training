16637d0b9bde3f8f639c7ac40928fced1db9401e
nasuheren <nasuherendemirci@gmail.com>
2024-04-01 16:58:51 +0300

    update

--- first_start.sh
+++ first_start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 # build docker image
-echo "docker build"
 docker build -t dockertraining .
 
 # image tag
-echo "docker image tag"
 docker tag dockertraining nasuheren/dockertraining:latest
 
 # push docker hub
-echo "docker push"
 docker push nasuheren/dockertraining:latest
 
 # deploy
-echo "deploy.sh"
 ./deploy.sh
